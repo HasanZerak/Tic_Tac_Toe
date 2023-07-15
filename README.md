a9ef4e284c37694af3e64a878e90636bcde5383f
Zauq <zerakhasanhashmi@gmail.com>
2023-07-15 20:47:21 +0530

    create two modules and a factory

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Tic_Tac_Toe
 A tic tac toe game to play with your friends or a bot.
+
+1. I began by structuring the HTML and adding basic CSS to the DOM. 
+2. Added a popup function to take user data.
+3. Created two modules, namely,
+    a. gameBoard, to manage the flow of the game and the logic.
+    b. display controller, to display the board and manipulate DOM elements. 
+4. Created a factory, 
+    a. player, to create two players and assign them a sign each. 
+5. gameboard has an array gameboard[] to store and run the game. 
--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-let popup = document.querySelectorAll(".popup");
-let decision_card_container = document.querySelector(
-  ".decision_card_container"
-);
-let decision_card = document.querySelector(".decision_card");
-let game_board = document.querySelector(".game_board");
+const player = (playerName, sign) => {
+  //assign signs to player
+  const getPlayerName = () => playerName;
+  const getPlayerSign = () => sign;
+};
 
-popup.forEach((btn) => {
-  btn.addEventListener("click", function () {
-    decision_card_container.removeChild(decision_card);
-    game_board.classList.add("open_game_board");
-    console.log(btn.textContent);
+const gameBoard = (() => {
+  //module for the game board
+  let board = ["", "", "", "", "", "", "", "", ""];
+
+  return { };
+})();
+
+const displayController = (() => {
+  //module for the display on screen
+  //cacheDOM
+  let popup = document.querySelectorAll(".popup");
+  let decision_card_container = document.querySelector(
+    ".decision_card_container"
+  );
+  let decision_card = document.querySelector(".decision_card");
+  let game_board = document.querySelector(".game_board");
+  let box = document.querySelectorAll(".box");
+
+  popup.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      decision_card_container.removeChild(decision_card);
+      game_board.classList.add("open_game_board");
+      // console.log(btn.textContent);
+    });
   });
-});
 
-// function openPopup(){
-//     popup.classList.add("open-decision-card");
-// }
+
+})();
+
