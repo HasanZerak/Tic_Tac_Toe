920f07e0518bcb9a81fbe6bb712e095731948297
Zauq <zerakhasanhashmi@gmail.com>
2023-07-16 12:41:48 +0530

    Print array to the board

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 A tic tac toe game to play with your friends or a bot.
 
 1. I began by structuring the HTML and adding basic CSS to the DOM. 
-2. Added a popup function to take user data.
+2. Added a popup function to take user data, that leads to the game board right after. 
 3. Created two modules, namely,
     a. gameBoard, to manage the flow of the game and the logic.
     b. display controller, to display the board and manipulate DOM elements. 
 4. Created a factory, 
     a. player, to create two players and assign them a sign each. 
 5. gameboard has an array gameboard[] to store and run the game. 
+
+6. Content of the gameBoard array is printed on the webpage using a for loop in the displayController module. 
--- script.js
+++ script.js
@@ -2,13 +2,15 @@ const player = (playerName, sign) => {
   //assign signs to player
   const getPlayerName = () => playerName;
   const getPlayerSign = () => sign;
+
+  return {getPlayerName, getPlayerSign};
 };
 
 const gameBoard = (() => {
   //module for the game board
-  let board = ["", "", "", "", "", "", "", "", ""];
+  let board = ["X","O","X","O","X","O","X","O","X"];     //gameBoard array to store player moves.
 
-  return { };
+  return { board };
 })();
 
 const displayController = (() => {
@@ -22,14 +24,17 @@ const displayController = (() => {
   let game_board = document.querySelector(".game_board");
   let box = document.querySelectorAll(".box");
 
-  popup.forEach((btn) => {
+  popup.forEach((btn) => {      //popup function to take sign input from the user
     btn.addEventListener("click", function () {
       decision_card_container.removeChild(decision_card);
       game_board.classList.add("open_game_board");
       // console.log(btn.textContent);
     });
   });
-
+  
+  for(let i = 0; i<gameBoard.board.length; i++){
+    box[i].textContent = gameBoard.board[i];
+  }
 
 })();
 
