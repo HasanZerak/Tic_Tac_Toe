3def4e4a7c1daf20463a582d41a25c07c1842ff9
Zauq <zerakhasanhashmi@gmail.com>
2023-07-18 16:15:28 +0530

    Allow players to place mark on the board

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
 # Tic_Tac_Toe
+
 A tic tac toe game to play with your friends or a bot.
 
-1. I began by structuring the HTML and adding basic CSS to the DOM. 
-2. Added a popup function to take user data, that leads to the game board right after. 
+1. I began by structuring the HTML and adding basic CSS to the DOM.
+2. Added a popup function to take user data, that leads to the game board right after.
 3. Created two modules, namely,
-    a. gameBoard, to manage the flow of the game and the logic.
-    b. display controller, to display the board and manipulate DOM elements. 
-4. Created a factory, 
-    a. player, to create two players and assign them a sign each. 
-5. gameboard has an array gameboard[] to store and run the game. 
+   a. gameBoard, to manage the flow of the game and the logic.
+   b. display controller, to display the board and manipulate DOM elements.
+4. Created a factory,
+   a. player, to create two players and assign them a sign each.
+5. gameboard has an array gameboard[] to store and run the game.
 
-6. Content of the gameBoard array is printed on the webpage using a for loop in the displayController module. 
+6. Content of the gameBoard array is printed on the webpage using a for loop in the displayController module.
+7. Add two loops, to:
+   a. first loop over the button nodelist and attach an event listener to each button.
+   b. when the user hits a button, execute function mark().
+   c. mark() loops over each box grid and finds the target div.
+   d. target div's text content is replaced with the last element in the gameBoard array.
+
+8. Make seperate functions for displaying, switching turns and checking conditions, namely:
+   a. displayMark() : display the latest change in the array. //displayController Module.
+   b. makeMove() : displayMark() calls this function to switch turns and check conditions. //gameBoard module.  
+   c. swiitchTurn() : switch turns and push the move into gameBoard aray. //gameBoard module.
--- script.js
+++ script.js
@@ -3,38 +3,79 @@ const player = (playerName, sign) => {
   const getPlayerName = () => playerName;
   const getPlayerSign = () => sign;
 
-  return {getPlayerName, getPlayerSign};
+  return { getPlayerName, getPlayerSign };
 };
 
 const gameBoard = (() => {
   //module for the game board
-  let board = ["X","O","X","O","X","O","X","O","X"];     //gameBoard array to store player moves.
+  let board = []; //gameBoard array to store player moves.
 
-  return { board };
+  let globalFlag = 0; //variable to decide the whose turn it is.
+  let player1 = player("player1", "X");
+  let player2 = player("player2", "O");
+
+  const switchTurn = (sign, flag, index) => {
+    //function to switch turns.
+    globalFlag = flag; //swaps the current player.
+    board.push(sign); //push the sign to the end of array
+    displayController.displayMark(index); //display the new array, func resides in the displayController module.
+  };
+
+  const makeMove = (index) => {
+    //fucntion to execute switch and check turn conditions. Called in the displayController Module.
+    if (globalFlag == 0) {
+      switchTurn("X", "1", index);
+      // console.log("does this even work??");
+    } else {
+      switchTurn("O", "0", index);
+      // console.log("what about this");
+    }
+  };
+
+  return { board, makeMove };
 })();
 
 const displayController = (() => {
   //module for the display on screen
   //cacheDOM
-  let popup = document.querySelectorAll(".popup");
-  let decision_card_container = document.querySelector(
-    ".decision_card_container"
-  );
-  let decision_card = document.querySelector(".decision_card");
-  let game_board = document.querySelector(".game_board");
+  // let popup = document.querySelectorAll(".popup");
+  // let decision_card_container = document.querySelector(
+  //   ".decision_card_container"
+  // );
+  // let decision_card = document.querySelector(".decision_card");
+  // let game_board = document.querySelector(".game_board");
   let box = document.querySelectorAll(".box");
+  let box_button = document.querySelectorAll(".box_button");
 
-  popup.forEach((btn) => {      //popup function to take sign input from the user
-    btn.addEventListener("click", function () {
-      decision_card_container.removeChild(decision_card);
-      game_board.classList.add("open_game_board");
-      // console.log(btn.textContent);
-    });
-  });
-  
-  for(let i = 0; i<gameBoard.board.length; i++){
-    box[i].textContent = gameBoard.board[i];
+  // popup.forEach((btn) => {
+  //   //popup function to take sign input from the user
+  //   btn.addEventListener("click", function () {
+  //     decision_card_container.removeChild(decision_card);
+  //     game_board.classList.add("open_game_board");
+  //     // console.log(btn.textContent);
+  //   });
+  // });
+
+  //function to display the sign.
+  const displayMark = (index) => {
+    box[index].textContent = gameBoard.board[gameBoard.board.length - 1]; //push the last entry in the gameBoard array onto the board.
+  };
+
+  for (let i = 0; i < box_button.length; i++) {
+    //loop over each button.
+    box_button[i].addEventListener("click", mark); //find the target button.
+
+    function mark() {
+      //function to respond when the target button is pressed & to display the sign on the board.
+
+      for (let j = 0; j < box.length; j++) {
+        //loop over each button, cross matching the target button and board.
+        if (i == j) {
+          gameBoard.makeMove(j); //exedutes makeMove() residing in the gameBoard Module, to decide the sign and player.
+        }
+      }
+    }
   }
 
+  return { displayMark };
 })();
-
